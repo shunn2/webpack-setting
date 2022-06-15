bc9a1ef37c5111b775399f8e2f54af5155e57cc2
cho seonghoon <chsida655@gmail.com>
2022-06-15 23:07:04 +0900

    react initial setting using webpack

---
+++ react/layouts/App/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+const App = () => {
+  return (
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Navigate replace to="/login" />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <div>hihihi@</div>
+  );
+};
+
+export default App;
